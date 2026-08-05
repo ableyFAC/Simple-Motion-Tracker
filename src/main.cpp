1f4dadeb71d6b2a124c7fa77600dbc1f0b1af913
Ethan <pumpkin673@gmail.com>
2026-08-05 15:35:29 -0700

    removed temperature in celcius, made every appropriate calculation with respect to farhenheit

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,32 @@
 // include any project and upload it
 // do NOT mix project. if you do, make a new project
-# include "sensor_proj.cpp"
+#include "sensor_proj.cpp"
+#include <LiquidCrystal.h>
 
 SensorProj sp;
-
+LiquidCrystal LCD(10, 9, 5, 4, 3, 2);
 void setup()
 {
     Serial.begin(9600);
     sp.activatePins();
-
+    LCD.begin(16, 2);
+    LCD.setCursor(0, 0);
+    LCD.print("distance:");
 }
 
 void loop()
 {
-
-    sp.temperatureF = sp.getTemperature();
-    sp.temperatureC = (sp.temperatureF - 32) * (5.00/9.00);
     
+    sp.getTemperature();
     sp.sendPing();
 
-    sp.time = pulseIn(sp.echoPin, HIGH) / 1000000.00;
-    sp.speedOfSound = (331.3 + (0.606 * sp.temperatureC)) * 39.3701; // in inches per second
-    sp.distance = (sp.speedOfSound * sp.time) / 2.00;
+    sp.setSpeedOfSound();
+    sp.setDistance();
 
-    sp.printTemperature();
-    sp.printDistance();
-
+    LCD.setCursor(0, 1);
+    LCD.print(sp.distance);
+    LCD.print(" inches");
+    delay(1000);
 }
 
 
--- src/sensor_proj.cpp
+++ src/sensor_proj.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+// uses the thermistor to get the temperature
+// then the temperature is used to calculate the speed of sound
+// finally the speed of sound is used to calculate the distance
+
 
 class SensorProj
 {
@@ -13,13 +17,16 @@ class SensorProj
         const double NOMINAL{25.00};
         const double B{3977.00};
 
+        const double FahrRate = 0.33;
+        const double baseSpeedFahr = 320.3;
+        const double inchesPerMeter = 39.3701;
+
         double time; // in microseconds
         double temperatureF;
-        double temperatureC;
         double speedOfSound; // in feet per second, it's 776.5 in mi/hr
         double distance;
     
-
+        // for void setup()
         void activatePins()
         {
             pinMode(triggerPin, OUTPUT); // we're writing to the trigger pin using the echo pin --> output
@@ -27,7 +34,8 @@ class SensorProj
             pinMode(thermistorPin, INPUT);
         }
 
-        double getTemperature() // returns in fahrenheit
+        // rest goes in void loop();
+        void getTemperature() // returns in fahrenheit
         {
             double thermVolt = (5.00 / 1023.00) * analogRead(thermistorPin);
             double resistorVolt = 5.00 - thermVolt;
@@ -35,9 +43,9 @@ class SensorProj
             double thermResistance = thermVolt / (resistorVolt / RESISTOR);
 
             double ln = log(thermResistance / THERMISTOR);
-            double temp = (1.00 / ((ln / B) + (1 / (NOMINAL + 273.15))));
+            double temp = (1.00 / ((ln / B) + (1 / (NOMINAL + 273.15)))); // in kelvins
 
-            return ((temp - 273.15) * (9.00/5.00)) + 32.00;
+            temperatureF = ((temp - 273.15) * (9.00/5.00)) + 32.00;
         }
 
         void printDistance()
@@ -46,17 +54,18 @@ class SensorProj
             Serial.print(distance);
             Serial.println(" inches away");
             delay(1000);
+            
         }
 
         void printTemperature()
         {
-            Serial.print("temperature is ");
+            Serial.print("temperature: ");
             Serial.print(temperatureF);
-            Serial.println(" *F ");
-            delay(1000);
+            Serial.println(" *F");
+
         }
 
-        void sendPing()
+        void sendPing() // sets the time it took for the ping to travel and return
         {
             digitalWrite(triggerPin, LOW);
             delayMicroseconds(2000); // delay by 2 milisecond
@@ -64,6 +73,17 @@ class SensorProj
             digitalWrite(triggerPin, HIGH);
             delayMicroseconds(10);
             digitalWrite(triggerPin, LOW); // trigger pulse ends here by bringing it low. by the end of this, a pulse was sent
+            time = pulseIn(echoPin, HIGH) / 1000000.00;
+        }
+
+        void setSpeedOfSound()
+        {
+            speedOfSound = (baseSpeedFahr + (FahrRate * temperatureF)) * inchesPerMeter; // in inches per second
+        }
+
+        void setDistance()
+        {
+            distance = (speedOfSound * time) / 2.00;
         }
 
 };
