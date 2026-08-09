7ec50be5cffdc85d7e862ba369761fb908168177
Ethan <pumpkin673@gmail.com>
2026-08-09 10:15:28 -0700

    made changes to sensor_proj class, added push button feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 // include any project and upload it
 // do NOT mix project. if you do, make a new project
 #include "sensor_proj.cpp"
-#include <LiquidCrystal.h>
 
-SensorProj sp;
-LiquidCrystal LCD(10, 9, 5, 4, 3, 2);
+SensorProj sp(10, 9, 5, 4, 3, 2);
+
 void setup()
 {
     Serial.begin(9600);
     sp.activatePins();
-    LCD.begin(16, 2);
-    LCD.setCursor(0, 0);
-    LCD.print("distance:");
 }
 
 void loop()
@@ -23,10 +19,5 @@ void loop()
     sp.setSpeedOfSound();
     sp.setDistance();
 
-    LCD.setCursor(0, 1);
-    LCD.print(sp.distance);
-    LCD.print(" inches");
-    delay(1000);
+    sp.displayInfo();
 }
-
-
--- src/sensor_proj.cpp
+++ src/sensor_proj.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
 // uses the thermistor to get the temperature
 // then the temperature is used to calculate the speed of sound
 // finally the speed of sound is used to calculate the distance
 
-
 class SensorProj
 {
     public:
-        const unsigned int triggerPin{13};
-        const unsigned int echoPin{11};
-        const unsigned int thermistorPin{A0};
+        LiquidCrystal LCD;
 
+        const uint8_t triggerPin{13};
+        const uint8_t echoPin{11};
+        const uint8_t thermistorPin{A0};
+        const uint8_t buttonPin{12};
+
+        uint8_t state = 0;
+        const uint8_t tempState = 0;
+        const uint8_t distState = 1;
+        
         const double RESISTOR{10000.00};
         const double THERMISTOR{10000.00};
 
@@ -26,12 +34,17 @@ class SensorProj
         double speedOfSound; // in feet per second, it's 776.5 in mi/hr
         double distance;
     
+        // constructor
+        SensorProj(uint8_t p1, uint8_t p2, uint8_t p3, uint8_t p4, uint8_t p5, uint8_t p6): 
+            LCD(p1, p2, p3, p4, p5, p6) {}
+
         // for void setup()
         void activatePins()
         {
             pinMode(triggerPin, OUTPUT); // we're writing to the trigger pin using the echo pin --> output
             pinMode(echoPin, INPUT); // we're reading from the echo pin --> input
-            pinMode(thermistorPin, INPUT);
+            pinMode(buttonPin, INPUT);
+            LCD.begin(16,2);
         }
 
         // rest goes in void loop();
@@ -46,23 +59,6 @@ class SensorProj
             double temp = (1.00 / ((ln / B) + (1 / (NOMINAL + 273.15)))); // in kelvins
 
             temperatureF = ((temp - 273.15) * (9.00/5.00)) + 32.00;
-        }
-
-        void printDistance()
-        {
-            Serial.print("closest object is ");
-            Serial.print(distance);
-            Serial.println(" inches away");
-            delay(1000);
-            
-        }
-
-        void printTemperature()
-        {
-            Serial.print("temperature: ");
-            Serial.print(temperatureF);
-            Serial.println(" *F");
-
         }
 
         void sendPing() // sets the time it took for the ping to travel and return
@@ -86,4 +82,56 @@ class SensorProj
             distance = (speedOfSound * time) / 2.00;
         }
 
+        void printDistance()
+        {
+            LCD.setCursor(0,0);
+            LCD.print("Distance: ");
+            LCD.setCursor(0,1);
+            LCD.print(distance);
+            LCD.print(" inches");
+            delay(1000);
+            
+        }
+
+        void printTemperature()
+        {
+            LCD.setCursor(0,0);
+            LCD.print("Temperature: ");
+            LCD.setCursor(0,1);
+            LCD.print(temperatureF);
+            LCD.print(" *F");
+            delay(1000);
+        }
+
+        void displayInfo()
+        {
+            // to implement: a push button
+            // if state == 0, print temperature
+            // if state == 1, print distance
+
+            if(digitalRead(buttonPin) == HIGH) // if the button was pressed
+            {
+                if(state == tempState) // if the current state is temperature, switch to distance
+                {
+                    state = distState;
+                }
+                else
+                {
+                    state = tempState;
+                }
+                LCD.clear();
+            }
+            
+            if(state == tempState)
+            {
+                
+                printTemperature();
+            }
+            else
+            {
+                LCD.clear();
+                printDistance();
+            }
+
+        }
 };
